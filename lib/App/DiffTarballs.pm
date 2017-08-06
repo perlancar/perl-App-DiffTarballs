36770a3c66e08fb0ad1eaf80685d3cfc5955d1da
perlancar (on pc-home) <perlancar@gmail.com>
2017-08-06 07:29:01 +0700

    Initial commit

---
+++ lib/App/DiffTarballs.pm
@@ -0,0 +1,78 @@
+package App::DiffTarballs;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+use File::chdir;
+use IPC::System::Options qw(system);
+
+our %SPEC;
+
+my %xcompletion_tarball = (
+    'x.completion' => [filename => {
+        filter => sub {-f $_[0] && $_[0] =~ /\.tar\.?/},
+    }],
+);
+
+$SPEC{diff_tarballs} = {
+    v => 1.1,
+    summary => 'Diff contents of two tarballs',
+    args => {
+        tarball1 => {
+            schema => 'filename*',
+            %xcompletion_tarball,
+            req => 1,
+            pos => 0,
+        },
+        tarball2 => {
+            schema => 'filename*',
+            %xcompletion_tarball,
+            req => 1,
+            pos => 1,
+        },
+    },
+    deps => {
+        all => [
+            {prog => 'tar'},
+            {prog => 'diff'},
+        ],
+    },
+    examples => [
+        {
+            argv => [qw/My-Dist-1.001.tar.gz My-Dist-1.002.tar.bz2/],
+            summary => 'Show diff between two Perl releases',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+    ],
+};
+sub diff_tarballs {
+    require Cwd;
+    require File::Temp;
+
+    my %args = @_;
+
+    my $abs_tarball1 = Cwd::abs_path($args{tarball1});
+    my $abs_tarball2 = Cwd::abs_path($args{tarball2});
+
+    my $dir1 = File::Temp::tempdir(CLEANUP => 1);
+    my $dir2 = File::Temp::tempdir(CLEANUP => 1);
+
+    $CWD = $dir1;
+    system({log=>1, die=>1}, "tar", "xf", $abs_tarball1);
+    system({log=>1, die=>1}, "tar", "xf", $abs_tarball2);
+    system("diff", "-ruN", $dir1, $dir2);
+    [200];
+}
+
+1;
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+See the included script L<diff-tarballs>.
